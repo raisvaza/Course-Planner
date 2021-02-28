5b2d9f4327cd550ae8088121ddf67d871a945031
Rais Vaza <raisvaza@gmail.com>
2021-02-28 22:27:50 +0700

    more text processing

--- fuck.py
+++
@@ -1,7 +0,0 @@
-f = open("Test.txt", 'r')
-mfile = f.readline()
-for i in range (len(mfile)):
-    mfile.split(",")
-for i in range (len(mfile)):
-    for j in (range(len(mfile[i]))):
-        print(mfile[i][j])
