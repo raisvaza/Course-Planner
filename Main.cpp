12fd7cc7d70f9367dfeee121f68774f5d6f7ebba
Rais Vaza <raisvaza@gmail.com>
2021-02-28 17:10:57 +0700

    text processing

--- Main.cpp
+++ Main.cpp
@@ -2,20 +2,52 @@
 #include "ListOfMatKul.hpp"
 #include <iostream>
 #include <fstream>
-
+#include <string>
 
 int main(){
     ListOfMatKul daftarMatKul();
-    string text;
-    string wholeText;
+    string baris = ""; // satu baris dalam text.txt
+    string wholeText = ""; // seluruh string yang diperoleh dari dalam text.txt
+    string kodeMatKul;
+    string prereq;
+    int n;
+    n = wholeText.size();
     fstream myText("Test.txt");
-    while (getline(myText, text))
-    {
-        wholeText = wholeText + " " + text + " \n";
+    while (getline(myText, baris))
+    {// Mengambil setiap baris dari text.txt
+        baris += '\n';
+        wholeText = wholeText + baris; // Meng-ekstrak seluruh text dari text.txt ke string wholeText        
     }
-
-    cout << wholeText;
-    cout << "Hello";
-    
+    int i = 0;
+    while (i < n)
+    {// Iterasi seluruh char di wholeText
+        while (wholeText[i] != '\n')
+        {// Iterasi char per baris
+            kodeMatKul = "";
+            while (wholeText[i] != ',')
+            {// Ambil kode matkul
+                kodeMatKul += wholeText[i];
+                i++;
+            }
+            cout << "Matkul: " << kodeMatKul << endl;
+            prereq = "";
+            while (wholeText[i] != '\n')
+            {// Ambil prereq
+                if (wholeText[i] == ',')
+                {
+                    i++;
+                }
+                
+                prereq += wholeText[i];
+                i++;
+            }
+            cout << "Prereq: " << prereq << endl;
+            
+            i++;
+        }
+        
+        i++;
+    }
+        
     return 0;
 }
