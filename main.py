1c04df1b784734e340538862ea0e37b1b23a5fbf
Rais Vaza <raisvaza@gmail.com>
2021-03-01 10:30:04 +0700

    finishing

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-def addToDaftar(namaMatkul, daftarMatkul):
+# CORNER (COURSE PLANNER)
+# Program Penyusunan Rencana Kuliah
+# Oleh: Rais Vaza Man Tazakka (13519060)
+
+def profil():
+# Memasukkan informasi profil pengguna
+# FITUR TAMBAHAN
+    nama = str(input("Masukkan nama Anda: "))
+    NIM = str(input("Masukkan NIM Anda: "))
+    fakultas = str(input("Masukkan fakultas Anda: "))
+    jurusan = str(input("Masukkan jurusan Anda: "))
+    print("Nama: " + nama)
+    print("NIM: " + NIM)
+    print("Fakultas: " + fakultas)
+    print("Jurusan: " + jurusan)
+
+def addToDaftar(namaMatkul, daftarMatkul): 
+# Menambah matkul ke daftar matkul
     i = 0
     while daftarMatkul[i][0] != "":
         i = i + 1
     temp = list(daftarMatkul[i])
     temp[0] = namaMatkul
-    temp[1] = 0
-    temp[2] = False
     daftarMatkul[i] = tuple((temp[0],int(temp[1]),temp[2]))
 
-def konversiAngka(angka):
+def konversiAngka(angka): 
+# Mengkonvesi angka arab menjadi angka romawi
     if angka == 1:
         return "I"
     elif angka == 2:
@@ -26,18 +42,40 @@ def konversiAngka(angka):
     elif angka == 8:
         return "VIII"
 
-def hapusMatkul(i,daftarMatkul, baris, semester):
-    if daftarMatkul[i][1] == 0:
+def hapusMatkul(i,daftarMatkul, baris, semester): 
+# Menghapus mata kuliah
+    n = semester + 1
+    if daftarMatkul[i][2] == False:        
+        if daftarMatkul[i][1] == 0:
+            print("Semester " + konversiAngka(semester) + ": " + daftarMatkul[i][0])
+            temp = list(daftarMatkul[i])
+            temp[0] = daftarMatkul[i][0]
+            temp[1] = daftarMatkul[i][1]
+            temp[2] = True
+            daftarMatkul[i] = tuple((temp[0],temp[1],temp[2]))
+            
+            # Mengurangi derajat masuk matkul yang memiliki daftarMatkul[i][0] sebagai prereq-nya
             for j in range(len(baris)):
                 for k in range(1,len(baris[j]),1):
                     if daftarMatkul[i][0] == baris[j][k]:
                         temp = list(daftarMatkul[j])
                         temp[1] -= 1
                         daftarMatkul[j] = tuple(temp)
-                        hapusMatkul(j,daftarMatkul,baris,semester+1)
-            temp = list(daftarMatkul[i])
-            temp[2] = True
-            print("Semester " + konversiAngka(semester) + ": " + daftarMatkul[i][0])
+
+            # Rekursi menghapus mata kuliah           
+            for j in range(len(baris)):
+                for k in range(1,len(baris[j]),1):
+                    if daftarMatkul[i][0] == baris[j][k]:
+                        if daftarMatkul[j][1] == 0: 
+                            hapusMatkul(j,daftarMatkul,baris,n)
+            
+        
+def isVisitedAll(daftarMatkul):
+# Mengecek apakah semua matkul sudah dikunjungi
+    visitedAll = True
+    for i in range(len(daftarMatkul)):
+        visitedAll = visitedAll and daftarMatkul[i][2]
+    return isVisitedAll
 
 # Mengubah file masukan menjadi array of array
 f = open("Test.txt", 'r')
@@ -64,7 +102,17 @@ for i in range(len(baris)):
         derajatMasuk += 1
         daftarMatkul[i] = tuple((daftarMatkul[i][0],derajatMasuk, False))
 
+# Menyambut pengguna
+print("\n               SELAMAT DATANG DI CORNER (COURSE PLANNER)")
+print("                SEBUAH PROGRAM PENYUSUNAN RENCANA KULIAH\n")
+print("-------------------------------------------------------------------------")
+
+# Menampilkan informasi pengguna
+profil()
+print("\nBerikut adalah daftar mata kuliah yang bisa Anda ambil sesuai dengan semesternya.")
+
+# TOPOLOGICAL SORT ATAS DAFTAR MATA KULIAH
 semester = 1
 for i in range(len(daftarMatkul)):
-    if daftarMatkul[i][2] == False:
-        hapusMatkul(i,daftarMatkul,baris,semester)
+    hapusMatkul(i,daftarMatkul,baris,semester)
+    i += 1
